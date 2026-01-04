cc72dc1bc8d19d1635081e42e7439b83296a3d5d
Emily Ochoa <eochoa057_est@instipp.edu.ec>
2026-01-03 23:08:54 -0500

    act alertas de error en html

--- apps/servicios/views.py
+++ apps/servicios/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from apps.servicios.models import Servicio
 from apps.especialidades.models import Especialidad
+from django.contrib.auth.decorators import login_required, user_passes_test
+from apps.usuarios.decorators import roles_permitidos
+from django.contrib import messages
+
 
 # Create your views here.
 
 @login_required
+@user_passes_test(roles_permitidos(['Administrador, Recepcionista']))
 def tabla_servicios(request):
     servicios = Servicio.objects.all()
     especialidades = Especialidad.objects.all()
@@ -15,6 +19,7 @@ def tabla_servicios(request):
         'especialidades': especialidades
     })
 
+@user_passes_test(roles_permitidos(['Administrador']))
 def registrar_servicio(request):
     especialidades = Especialidad.objects.all()
     if request.method == 'POST':
@@ -33,9 +38,11 @@ def registrar_servicio(request):
             costo = costo,
             descripcion = descripcion
         )
+        messages.success(request, 'Servicio registrado correctamente.')
         return redirect('tabla_servicios')
     return render(request, 'servicios/tabla_servicios.html', {'especialidades': especialidades})
 
+@user_passes_test(roles_permitidos(['Administrador']))
 @login_required
 def editar_servicio(request, servicio_id):
     servicio = get_object_or_404(Servicio, id=servicio_id)
@@ -50,14 +57,15 @@ def editar_servicio(request, servicio_id):
         servicio.descripcion = request.POST.get('txtDescripcion')
 
         servicio.save()
-    
+        messages.success(request, 'Servicio actualizado correctamente.')
         return redirect('tabla_servicios')
 
     return render(request, 'servicios/editar_servicio.html', {'servicio': servicio, 'especialidades': especialidades})
 
+@user_passes_test(roles_permitidos(['Administrador']))
 @login_required
 def eliminar_servicio(request, servicio_id):
     servicio = get_object_or_404(Servicio, id=servicio_id)
     servicio.delete()  
-    
+    messages.success(request, 'Servicio eliminado correctamente.')
     return redirect('tabla_servicios')
