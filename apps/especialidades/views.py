8c97d37bfae18dfd5f6aa0229ecc03be35e52678
Emily Ochoa <eochoa057_est@instipp.edu.ec>
2026-01-03 23:10:32 -0500

    act views especialidades

--- apps/especialidades/views.py
+++ apps/especialidades/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required, user_passes_test
 from apps.especialidades.models import Especialidad
 from apps.usuarios.decorators import roles_permitidos
+from django.contrib import messages
 
 # Create your views here.
 
@@ -20,8 +21,9 @@ def registrar_especialidad(request):
         especialidadCreate = Especialidad.objects.create(
             nombre = nombre
         )
+        messages.success(request, 'Especialidad registrada correctamente.')
         return redirect('tabla_especialidades')
-    return render(request, 'especialidades/tabla_especialidades.html')
+    return redirect('tabla_especialidades')
 
 @login_required
 @user_passes_test(roles_permitidos(['Administrador']))
@@ -31,7 +33,7 @@ def editar_especialidad(request, especialidad_id):
     if request.method == 'POST':
         especialidad.nombre = request.POST.get('txtNombre')
         especialidad.save()
-    
+        messages.success(request, 'Especialidad actualizada correctamente.')
         return redirect('tabla_especialidades')
 
     return render(request, 'especialidades/editar_especialidad.html', {'especialidad': especialidad})
@@ -43,5 +45,5 @@ def eliminar_especialidad(request, especialidad_id):
 
     especialidad.delete()  
     
-    
+    messages.success(request, 'Especialidad eliminada correctamente.')
     return redirect('tabla_especialidades')
