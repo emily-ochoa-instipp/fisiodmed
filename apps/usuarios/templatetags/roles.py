2235b5f3d47bc4823fadba1c4b22875303c8cd87
Emily Ochoa <eochoa057_est@instipp.edu.ec>
2026-01-07 12:02:21 -0500

    act roles templatetag

--- apps/usuarios/templatetags/roles.py
+++ apps/usuarios/templatetags/roles.py
@@ -2,14 +2,10 @@ from django import template
 
 register = template.Library()
 
-@register.filter
+@register.filter(name='has_group')
 def has_group(user, group_names):
-    """
-    Uso:
-    {% if request.user|has_group:"Secretaria,Presidenta,'Administrador' %}
-    """
     if not user.is_authenticated:
         return False
 
-    groups = group_names.split(',')
+    groups = [g.strip() for g in group_names.split(',')]
     return user.groups.filter(name__in=groups).exists()
