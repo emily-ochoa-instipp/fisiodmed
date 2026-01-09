d9e227716f43a445d3c33b22664a7968d43fd83e
Emily Ochoa <eochoa057_est@instipp.edu.ec>
2026-01-09 03:53:47 -0500

    eliminacion de app dashboard y reportes

--- apps/reportes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # Vistas de reportes 
-    path('editar_reporte/', views.editar_reporte, name='editar_reporte'),
-    path('tabla_reportes/', views.tabla_reportes, name='tabla_reportes'),
-]
