a2922463d15488f240fe7aa1c89e6edd262db23f
Emily Ochoa <eochoa057_est@instipp.edu.ec>
2026-01-12 02:40:28 -0500

    act pass defecto en views usuarios

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -59,7 +59,7 @@ def registrar_usuario(request):
                 last_name=last_name_,
                 email=email_,
                 username=username_,
-                password=password_
+                password='12345'
             )
 
             user.is_active = True   # Activo por defecto
